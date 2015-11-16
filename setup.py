2f7cf0b3a925279fd75b39035a66232049dd063f
Jared Lewis <jared@jared.kiwi.nz>
2015-11-17 11:50:30 +1300

    bug: invalid parameter to open

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from credkeep import __version__
 from setuptools import setup, find_packages
 from os import path
 
-with open(path.join(path.abspath(path.dirname(__file__)), 'README.rst'), encoding='utf-8') as f:
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.rst')) as f:
     long_description = f.read()
 
 setup(
